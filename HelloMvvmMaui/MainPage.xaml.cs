28ad4f3d0ba6a3db19e32e937c0cb5b5f1030341
Carla Taiany Oliveira <carlataianyoj@gmail.com>
2025-09-10 17:35:41 -0300

    Adicionar arquivos de projeto.

---
+++ HelloMvvmMaui/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using HelloMvvmMaui.ViewModels;
+namespace HelloMvvmMaui
+{
+    public partial class MainPage : ContentPage
+    {
+       
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            //Define o contexto de dados da View (MainPage.xaml) para o ViewModel (MainViewModel.cs)
+            BindingContext = new ViewModels.MainViewModel(); 
+        }
+
+        
+    }
+}
---
+++ HelloMvvmMaui/ViewModels/MainViewModel.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.Maui.Controls;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.Intrinsics.X86;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace HelloMvvmMaui.ViewModels
+{
+    //ViewModel para conversar com  a MainPage.xaml (minha View)
+   public class MainViewModel: INotifyPropertyChanged
+    //Quando uma propriedade mudar, a View (MainPage.xaml) será notificada
+   {
+        private string _nome;
+        private string _saudacao;
+
+        //Propreidade que vai conversar com o nosso Entry (MainPage.xaml)
+        public string Nome
+        {
+            get => _nome;
+            set 
+            { 
+                if(_nome != value)
+                {
+                    _nome = value;
+                    OnPropetyChanged(); //Notifica a View que a propriedade mudou
+
+                    CumprimentarCommand.ChangeCanExecute(); //Atualiza o estado do comando
+                }                
+            }
+        }
+
+        //Propreidade que vai conversar com o nosso Label (MainPage.xaml)
+        public string Saudacao
+        {
+            get => _saudacao;
+            set 
+            { 
+                if(_saudacao != value)
+                {
+                    _saudacao = value;
+                    //Notifica a View que a propriedade mudou
+                    OnPropetyChanged(); 
+                }                
+            }
+        }
+
+        public Command CumprimentarCommand { get; }
+        //Comando para o botão Cumprimentar
+        
+        public MainViewModel()
+        {
+            //Define o que o Cammand faz e qaundo executar
+            CumprimentarCommand = new Command(execute: () =>
+            {
+                //Ação do comando de mostrar a saudação
+                Saudacao = $"Olá, {Nome}!";
+            },
+
+            //Habilita o boatão se o Nome não for vazio ou nulo
+            canExecute: () => !string.IsNullOrWhiteSpace(Nome) 
+            );
+        }
+
+        //Suporte do meu biding
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropetyChanged([CallerMemberName] string propName = null) =>
+            PropertyChanged?.Invoke (this, new PropertyChangedEventArgs(propName));
+           
+   
+        
+            
+
+
+        }
+
+}
